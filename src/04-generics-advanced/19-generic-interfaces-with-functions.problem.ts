722f73e9c5a95162c9fbaebedcfb06cc83800a7a
Philip Fuster <pafus23@protonmail.com>
2024-12-18 22:53:36 -0500

    04-generics-advanced ex 19

--- src/04-generics-advanced/19-generic-interfaces-with-functions.problem.ts
+++ src/04-generics-advanced/19-generic-interfaces-with-functions.problem.ts
@@ -5,7 +5,7 @@ export interface Cache<T> {
   get: (key: string) => T | undefined;
   set: (key: string, value: T) => void;
   // You can fix this by only changing the line below!
-  clone: (transform: (elem: unknown) => unknown) => Cache<unknown>;
+  clone: <TNewCache>(transform: (elem: T) => TNewCache) => Cache<TNewCache>;
 }
 
 const createCache = <T>(initialCache?: Record<string, T>): Cache<T> => {
