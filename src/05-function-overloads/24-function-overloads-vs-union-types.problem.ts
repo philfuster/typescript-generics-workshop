60a8dd82a699222f5bbf11fe51cb16c8aff93405
Philip Fuster <pafus23@protonmail.com>
2024-12-20 18:03:52 -0500

    05-function-overloads ex 24

--- src/05-function-overloads/24-function-overloads-vs-union-types.problem.ts
+++ src/05-function-overloads/24-function-overloads-vs-union-types.problem.ts
@@ -1,7 +1,15 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-function runGenerator(generator: unknown) {
+// function runGenerator(generator: () => string): string;
+// function runGenerator(generator: { run: () => string }): string
+// function runGenerator(generator: { run: () => string } | (() => string)): string {
+//   if (typeof generator === "function") {
+//     return generator();
+//   }
+//   return generator.run();
+// }
+function runGenerator(generator: { run: () => string } | (() => string)): string {
   if (typeof generator === "function") {
     return generator();
   }
