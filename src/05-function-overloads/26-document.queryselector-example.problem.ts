b96463629c6bcdda827a3a6eb3bf27aa950af97f
Phil Fuster <pafus23@protonmail.com>
2024-12-27 10:57:56 -0500

    formatted text in ex 26 document.querySelector example
    Using lazygit for first time. this is pretty cool.
    I didn't know you could have a summary and description.
    I am excited to see what this looks like actually lol

--- src/05-function-overloads/26-document.queryselector-example.problem.ts
+++ src/05-function-overloads/26-document.queryselector-example.problem.ts
@@ -12,5 +12,5 @@ const divElement2 = document.querySelector<HTMLDivElement>("div.foo");
 type tests = [
   Expect<Equal<typeof divElement, HTMLDivElement | null>>,
   Expect<Equal<typeof spanElement, HTMLSpanElement | null>>,
-  Expect<Equal<typeof divElement2, HTMLDivElement | null>>
+  Expect<Equal<typeof divElement2, HTMLDivElement | null>>,
 ];
